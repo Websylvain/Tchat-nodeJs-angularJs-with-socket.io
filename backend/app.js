84da3b7214b8a0a937f96fc89cca32d007e8a10d
websylvain <contact@websylvain.com>
2017-08-31 14:24:14 +0200

    Keep messages in a collection

--- backend/app.js
+++ backend/app.js
@@ -1,67 +1,90 @@
+//INIT
 var http = require('http');
 var md5 = require('MD5');
-
+var mongoClient = require('mongodb').MongoClient;
 httpServer = http.createServer(function(req,res){
     console.log('Connection d\'un client !');
     res.end('hello !');
 });
-
 httpServer.listen(9999);
 var io = require('socket.io').listen(httpServer);
+var url = 'mongodb://127.0.0.1:27017/mongochat';
+//END INIT
 
-var users = [];
-var messages = [];
-
-// EDIT CONNECTION
-io.sockets.on("connection",function(socket){
-
-    console.log("nouveau utilisateur connecté");
-
-    //LIST THE USERS logged
-    for (var k in users) {
-        socket.emit("addedUser", users[k]);
+//DB CONNECTION
+mongoClient.connect(url,function(err,db){
+    if(err){
+        throw err;
     }
 
-    var current_user = false;
+    console.log('DB is okay');
+    let users = [], messages = [] , current_user = false;
 
-    //LIST MESSAGES 
-    for (var m in messages) {
-        socket.emit("addedMessage", messages[m]);
-    }
+    io.sockets.on('connection',function(socket){
+        console.log("A new client is connected to app.js");
 
-    var current_user = false;
+        db.collection('messages').find().toArray(function(err, res){
+            if(err){
+                throw err;
+            }
 
-    //EVENT SUBSCRIBE
-    socket.on('subscribe',function(user){
-        // USE INFO FROM FRONT END
-        current_user = user;
-        console.log(user);
-        current_user.id = user.mail.replace("@",'-').replace(".","-");
-        current_user.avatar = "http://gravatar.com/avatar/" +md5(user.mail) + "?s=50";
+            messages = res;
+
+            //LIST MESSAGES 
+            for (var m in messages) {
+                socket.emit("addedMessage", messages[m]);
+            }
+
+        });
         
-        //Add user to users list
-        users[current_user.id] = current_user;
         
-        //Log the current user
-        socket.emit("newusr",current_user);
-        //Say @ every one that new usr is arrived
-        io.sockets.emit("addedUser",current_user);
-    });
-
-    //EVENT DISCONNECT
-    socket.on('disconnect', function(){
-        if(!current_user){
-            return false
+        //LIST USERS logged
+        for (var k in users) {
+            socket.emit("addedUser", users[k]);
         }
-        delete users[current_user.id];
-        io.sockets.emit("deleteUsrFromList", current_user);
-    })
+        
+        //EVENT SUBSCRIBE
+        socket.on('subscribe',function(user){
+            // USE INFO FROM FRONT END
+            current_user = user;
+            console.log(user);
+            current_user.id = user.mail.replace("@",'-').replace(".","-");
+            current_user.avatar = "http://gravatar.com/avatar/" +md5(user.mail) + "?s=50";
+            
+            //Add user to users list
+            users[current_user.id] = current_user;
+            
+            //Log the current user
+            socket.emit("newusr",current_user);
+            //Say @ every one that new usr is arrived
+            io.sockets.emit("addedUser",current_user);
+        });
 
-    //EVENT MESSAGE
-    socket.on("sendMessage",function(msgInfo){
-        console.log(msgInfo.user.login + ": " + msgInfo.message );
-        messages.push(msgInfo);
-        io.sockets.emit("addedMessage", msgInfo);
+        //EVENT DISCONNECT
+        socket.on('disconnect', function(){
+            if(!current_user){
+                return false
+            }
+            delete users[current_user.id];
+            io.sockets.emit("deleteUsrFromList", current_user);
+        });
+
+        //EVENT MESSAGE
+        // msgInfo = {"user":"ddd","message":"mon message"}
+        socket.on("sendMessage",function(msgInfo){
+            console.log(msgInfo);
+            //Push Message in Array
+            messages.push(msgInfo);
+            //Insert Message in collection
+            db.collection('messages').insert(msgInfo);
+            //Add message to client
+            io.sockets.emit("addedMessage", msgInfo);
+        });
+
+
+
     });
-    
+
+
 });
+
--- frontend/app/ctrl/subscribeForm.js
+++ frontend/app/ctrl/subscribeForm.js
@@ -1,6 +1,7 @@
 app.controller('SubscribeForm',["$scope", "socket","settings", function($scope, socket, settings) {
     $scope.login = false;
 
+    //SERVER RETURN TO YOU
     socket.on('newusr',function(userInfo){
         $scope.login = true;
         settings.setUSER(userInfo);
